2a92a487dc4dcab151cd9a3f74d93c2beb225f0e
Rama Linga Reddy Nandyal <rama.nandyal@shellinfoglobal.com>
2026-03-16 10:16:27 +0530

    Added what's app message

---
+++ src/main/java/com/shellinfo/demo/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.shellinfo.demo.controller;
+
+import com.shellinfo.demo.service.OtpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final OtpService otpService;
+
+    @PostMapping("/send-otp")
+    public String sendOtp(@RequestParam String mobile) {
+
+        String oto = otpService.sendOtp(mobile);
+
+        return "OTP sent successfully";
+    }
+}
